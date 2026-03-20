8c5c4025f26d41d429d77e8e0d0ba2b2f5baca6a
seajihey <okcoco03@naver.com>
2026-03-20 14:46:27 +0900

    f

---
+++ src/main/java/edu/fisa/ce/Step06BuildGradleTest1Application.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step06BuildGradleTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step06BuildGradleTest1Application.class, args);
+	}
+
+}
